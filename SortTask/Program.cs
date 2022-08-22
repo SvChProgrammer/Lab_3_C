7eae79e40dd1700450bd6497b34d9404364c0086
Svetlana <lana.chebotareva.95@mail.ru>
2022-08-22 15:16:37 +0400

    Task Bubble_sort

---
+++ SortTask/Program.cs
@@ -0,0 +1,49 @@
+﻿int[] BubbleSort(int[] array)
+{
+  for (int i = 0; i < array.Length; i++){
+      
+      for (int j = 0; j < array.Length -1; j++)
+      {
+           if (array[j] > array[j+1])
+           {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+           }
+       }
+    Console.Write(array[i] + " ");
+  }               
+    return array;       
+}
+
+Console.WriteLine("Укаждите число элементов массива: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i<size; i++) {
+
+        array[i] = new Random().Next(0, 100000);
+    }
+    return array;
+}
+
+int[] array = GetArray(n);
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for(int i = 0; i <array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+PrintArray(array);
+BubbleSort(array);
+
