31f4a02717c8c8e43bc5218e33f5fa3faca5413d
Svetlana <lana.chebotareva.95@mail.ru>
2022-08-24 07:59:06 +0400

    task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -9,7 +9,7 @@ int[] GetArray(int size)
 
     for(int i = 0; i<size; i++)
     {
-        array[i] = new Random().Next(100, 1001);
+        array[i] = new Random().Next(100, 1000);
     }
     return array;
 }
